1c70ed6d3b0caaa0610679ffbffbfd20ec5e03f8
Juan Camilo <jcamilomc21@gmail.com>
2024-08-27 20:53:06 -0500

    Ejercicio

---
+++ Ejercicio.java
@@ -0,0 +1,22 @@
+public class Ejercicio {
+    float salarioSinAjuste;
+    float aumento;
+    float SalarioConAjuste;
+    public Ejercicio(float salarioSinAjuste, float aumento, float SalarioConAjuste) {
+        this.salarioSinAjuste=0;
+        this.aumento=0;
+        this.SalarioConAjuste= 0;
+    
+    }
+    public float calcularSalario(){
+        if (salarioSinAjuste<1300606) {
+            
+        } else {
+            
+        }
+
+
+        return SalarioConAjuste;
+
+    }
+}
